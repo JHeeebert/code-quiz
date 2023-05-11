62e91dcc81d8c5d9767ed969cef8d852f969c9e3
Josh Hebert <jhebertcocktails@gmail.com>
2023-05-10 21:55:17 -0400

    layout for JS file finished HTML and CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-var timerCount = 60;
-var startButton = document.querySelector(".startButton");
-var timer= document.querySelector(".timer")
+//Create Click event for start button that starts the game
+//Timer starts 
 
-startButton.addEventListener("click", function (event) {
-        startButton.style.display= none 
-    
-    
-    }
-)
 
+//when timer starts the game should display the first question and the multiple choice answers 
+
+
+//if i answer wrong time is taken from clock
+
+
+//when all questions are answered or timer reaches 0 the game ends 
+
+
+//when games ends you should be prompted to save score and initials \
+
+
+
+//after sending initials and high score you shoulkd be able to restart or clear your high scores
